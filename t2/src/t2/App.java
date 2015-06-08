4ef4666e430d1049b4cfeaad57e6a88a3f165a04
anderson <anderson.civil@hotmail.com>
2015-06-08 20:03:55 -0300

    feito alterações na classe App

--- t2/src/t2/App.java
+++ t2/src/t2/App.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 
 public class App {
 
+	@SuppressWarnings("static-access")
 	public static <E> void main(String[] args) throws IOException {
-		// ConversionBetweenBases c = new ConversionBetweenBases();
-		System.out.println(ConversionBetweenBases.convert(7));
+		ConversionBetweenBases c = new ConversionBetweenBases();
+		//System.out.println("Converter numero 7 em decimal para base 6: " + ConversionBetweenBases.convert(7));
 		Reader<E> r = new Reader<E>();
 		r.loadTestCase();
 		System.out.println("Tamanho: " + r.list.size());
 		System.out.println("Get posição 1: " + r.list.get(1));
+		System.out.println("Converter numero 107 em decimal para base 6: " + c.convert(107));
 
 	}
 
