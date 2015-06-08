71f23dc902f3d69aa63b7d015b0cf951724ade74
anderson <anderson.civil@hotmail.com>
2015-06-08 17:28:40 -0300

    primeiro commit

---
+++ t2/src/t2/App.java
@@ -0,0 +1,11 @@
+package t2;
+
+public class App {
+	
+	public static void main(String[] args) {
+		//ConversionBetweenBases c = new ConversionBetweenBases();
+		System.out.println(ConversionBetweenBases.converteDecimalParaBase6(10000));
+
+	}
+
+}
---
+++ t2/src/t2/ConversaoEntreBases.java
@@ -0,0 +1,98 @@
+package t2;
+
+/**********************************************************
+ * Programa: programa em Java que demonstra a
+ *           conversão de números entre bases diferentes
+ *
+ * Compilação: javac ConversaoEntreBases.java
+ * Execução: java ConversaoEntreBases
+ *
+ * % java ConversaoEntreBases
+ * % ---------------------------------
+ * % Decimal 211 para Binário: 11010011
+ * % Decimal 211 para Octal: 323
+ * % Decimal 211 para Hexadecimal: d3
+ * % ---------------------------------
+ * % Binário: 11101110 para Decimal: 238
+ * % Binário: 11101110 para Octal: 356
+ * % Binário: 11101110 para Hexadecimal: ee
+ * % ---------------------------------
+ * % Octal: 77 para Decimal: 63
+ * % Octal: 77 para Binário: 111111
+ * % Octal: 77 para Hexadecimal: 3f
+ * % ---------------------------------
+ * % Hexadecimal: 58 para Decimal: 88
+ * % Hexadecimal: 58 para Binário: 1011000
+ * % Hexadecimal: 58 para Octal: 130
+ *
+ * @author Marcos Cesar Kossoski
+ *
+**********************************************************/
+public class ConversaoEntreBases {
+
+  public static void main (String[] args) {
+
+    String dec = "211";
+    // string para inteiro decimal
+    int decimal = Integer.parseInt(dec, 10);
+    // decimal para binario
+    String binario = Integer.toBinaryString(decimal);
+    // decimal para octal
+    String octal = Integer.toOctalString(decimal);
+    // decimal para hexadecimal
+    String hexadecimal = Integer.toHexString(decimal);
+    //decimar para base 6
+   // String baseSeis = Integer.to
+
+    System.out.println("---------------------------------");
+    System.out.println("Decimal " + dec + " para Binário: " + binario);
+    System.out.println("Decimal " + dec + " para Octal: " + octal);
+    System.out.println("Decimal " + dec + " para Hexadecimal: " + hexadecimal);
+
+    // -------------------------------------------------------
+
+    String bin = "11101110";
+    // string binario para decimal
+    int decBin = Integer.parseInt(bin, 2);
+    // binario para octal
+    String decOct = Integer.toOctalString(decBin);
+    // binario para hexadecimal
+    String decHex = Integer.toHexString(decBin);
+
+    System.out.println("---------------------------------");
+    System.out.println("Binário: " + bin + " para Decimal: " + decBin);
+    System.out.println("Binário: " + bin + " para Octal: " + decOct);
+    System.out.println("Binário: " + bin + " para Hexadecimal: " + decHex);
+
+    // -------------------------------------------------------
+
+    String oct = "77";
+    // string octal para inteiro decimal
+    int octalInt = Integer.parseInt(oct, 8);
+    // octal para hexadecimal
+    String octHex = Integer.toHexString(octalInt);
+    // octal para binário
+    String octBin = Integer.toBinaryString(octalInt);
+
+    System.out.println("---------------------------------");
+    System.out.println("Octal: " + oct + " para Decimal: " + octalInt);
+    System.out.println("Octal: " + oct + " para Binário: " + octBin);
+    System.out.println("Octal: " + oct + " para Hexadecimal: " + octHex);
+
+    // -------------------------------------------------------
+
+    String hex = "58";
+    // string hexadecimal para inteiro decimal
+    int hexInt = Integer.parseInt(hex, 16);
+    // hexadecimal para octal
+    String hexOct = Integer.toOctalString(hexInt);
+    // hexadecimal para binario
+    String hexBin = Integer.toBinaryString(hexInt);
+
+    System.out.println("---------------------------------");
+    System.out.println("Hexadecimal: " + hex + " para Decimal: " + hexInt);
+    System.out.println("Hexadecimal: " + hex + " para Binário: " + hexBin);
+    System.out.println("Hexadecimal: " + hex + " para Octal: " + hexOct);
+  }
+
+}
